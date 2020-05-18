9b91146f87319e76b2f4effcf4039a58bbf2116f
takesyun0611 <take.shun1616@gmail.com>
2020-05-18 22:02:12 +0900

    Automatic updata function Create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,22 +3,24 @@ $(function(){
     if ( message.image ) {
       var html =
      `<div class="mainchat__message">
-          ${message.user_name}
-        <a class="mainchat__message-form">
-          <a class="mainchat__Times-of-Day">
-            ${message.created_at}
-          <br>
+        <div class="message" data-message-id=${message.id}></div>
+            ${message.user_name}
+          <a class="mainchat__message-form">
+            <a class="mainchat__Times-of-Day">
+              ${message.created_at}
+            <br>
+            </a>
+            <div class="lower-message__content">
+              ${message.content}
+            </div>
+              <img src=${message.image} >
           </a>
-          <div class="lower-message__content">
-            ${message.content}
-          </div>
-            <img src=${message.image} >
-        </a>
       </div>`
     return html;
   } else {
     var html =
     `<div class="mainchat__message">
+      <div class="message" data-message-id=${message.id}></div>
           ${message.user_name}
         <a class="mainchat__Times-of-Day">
           ${message.created_at}
@@ -31,6 +33,7 @@ $(function(){
     return html;
   };
 }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
    var formData = new FormData(this);
@@ -53,6 +56,39 @@ $(function(){
    .fail(function() {
     alert("メッセージ送信に失敗しました");
     setTimeout("location.reload()",0);
+    });
   });
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.mainchat__message').append(insertHTML);
+      $('.mainchat__message').animate({ scrollTop: $('.mainchat__message')[0].scrollHeight});
+    }
   })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
